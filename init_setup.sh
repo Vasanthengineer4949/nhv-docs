b15af98f3ea76497fb3574c8a672351177b13d21
Vasanth <vasanthp.cse2019@citchennai.net>
2023-08-25 15:59:03 +0530

    license added

--- init_setup.sh
+++ init_setup.sh
@@ -2,9 +2,6 @@ echo [$(date)]: "START"
 export _VERSION_=3.9
 echo [$(date)]: "creating environment with python ${_VERSION_}"
 conda create --prefix ./env python=${_VERSION_} -y
-# echo [$(date)]: "add git user"
-# git config user.name "c17hawke"
-# git config user.email "sunny.c17hawke@gmail.com"
 echo [$(date)]: "activate environment"
 source activate ./env
 echo [$(date)]: "install requirements"
